438ffa027178034afd01f251582e38134b9d40ed
pooja19purohit <poojaap66@gmail.com>
2016-06-29 17:02:41 -0400

    Added Employee base class

---
+++ src/com/insticator/model/Employee.java
@@ -0,0 +1,57 @@
+package com.insticator.model;
+
+public abstract class Employee {
+	protected String SSN;
+	protected String firstName;
+	protected String lastName;
+	protected double salary;
+	protected String employeeType;
+	
+	protected static enum EmployeeType
+	{
+	    Contract,
+	    FullTime,
+	    PartTime,
+	    Intern
+	}
+	
+	Employee(String SSN,String firstName,String lastName,double salary, EmployeeType type) {
+		this.SSN = SSN;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+		this.employeeType = type.toString();
+	}
+	
+	public String getSSN() {
+		return SSN;
+	}
+	public void setSSN(String sSN) {
+		SSN = sSN;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getEmployeeType() {
+		return employeeType;
+	}
+
+	public void setEmployeeType(String employeeType) {
+		this.employeeType = employeeType;
+	}
+}
